5dbec88c54b4112d58b83b79010508f4d7c5499a
DimamonDESKTOP <dimamon02@gmail.com>
2024-05-07 13:43:28 +0300

    Unnecessary comments removed

--- DraftBook/MainWindow.xaml.cs
+++ DraftBook/MainWindow.xaml.cs
@@ -54,17 +54,14 @@ namespace DraftBook
 
             try
             {
-                // Перевіряємо, чи вказано ім'я файлу
                 if (string.IsNullOrEmpty(fileName))
                 {
                     MessageBox.Show("No file selected.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                     return;
                 }
 
-                // Створюємо новий файл і записуємо вміст
                 File.WriteAllText(filePath, fileContent);
 
-                // Додаємо інформацію про файл до колекції для відображення у DataGrid
                 Files.Add(new FileInformation(fileName, fileContent.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries).Length, DateTime.Now));
 
                 MessageBox.Show("File created successfully.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
